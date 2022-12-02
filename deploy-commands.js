4ce9d43fe297a3d2c594fa5f392cb0ab5035f562
CraigSau <37848439+CraigSau@users.noreply.github.com>
2022-12-02 14:23:01 -0600

    Added deploy-commands, added initial commands logic

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+const fs = require('node:fs');
+dotenv.config();
+const token = process.env.DISCORD_TOKEN;
+const appId = process.env.APP_ID;
+const guildId = process.env.GUILD_ID;
+
+const commands = [];
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        const data = await rest.put(
+            Routes.applicationGuildCommands(appId), { body: commands }
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
