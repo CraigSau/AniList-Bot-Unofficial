fa9c760a3d280f5668c85d69b6d7b7ca5faceb1b
Craig <craigsauers@ymail.com>
2023-01-30 13:37:26 -0600

    Get Data from AnilistAPI / Axios added
    Successfully get data from AnilistAPI, next step is to figure out how to split the data returned in the JSON object apart.

--- anilistAPI.js
+++ anilistAPI.js
@@ -1,44 +1,22 @@
-const query = require('./queries/user');
+const axios = require('axios');
 
-async function getAnime(animeQuery) {
+function getAnime(query, variables) {
 
-
-    let variables = {
-        search: query,
-        pages: 1,
-        perPage: 3,
+    var options = {
+        method: 'POST',
+        url: 'https://graphql.anilist.co',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        data: {
+            query: query,
+            variables: variables
+        }
     };
 
-    var url = 'https://graphql.anilist.co',
-        options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({
-                query: query,
-                variables: variables
-            })
-        };
 
-
-    fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
-
-    function handleResponse(response) {
-        return response.json().then(function (json) {
-            return response.ok ? json : Promise.reject(json);
-        });
-    }
-
-    function handleData(data) {
-        console.log(data);
-    }
-
-    function handleError(error) {
-        alert('Error, check console');
-        console.error(error);
-    }
+    return axios(options).then((response) => response.data).catch((err) => console.log(err));
 }
 
-module.exports = getAnime.fetch;
+module.exports = getAnime;
--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,6 @@
-const api = require("../anilistAPI");
+const anilistApi = require("../anilistAPI");
 const query = require("../queries/user");
-const discordMessage = require("../discordMessage");
-const striptags = require("striptags");
-const axios = require("axios");
-const { SlashCommandBuilder, SlashCommandStringOption } = require("discord.js");
-const { request } = require("express");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 
 // PROBLEMS
@@ -22,50 +18,65 @@ const { request } = require("express");
 
 const data = new SlashCommandBuilder()
     .setName('user')
-    .setDescription('Get info about a users AniList profile')
-    .add
+    .setDescription('Get info about a users AniList profile');
 
 
-const search = async searchArg => {
-    const response = await api(query, {
-        search: searchArg
-    });
-
-    if (response.error) {
-        return response;
-    }
-
-    const data = response.User;
-    console.log(data);
-    const watchedTime = data.statistics.anime.minutesWatched;
-    const chaptersRead = data.statistics.manga.chaptersRead;
-
-    const chaptersString = chaptersRead != 0 ? `Chapters read ${chaptersRead}` : "";
-
-    let daysWatched = "";
-    if (watchedTime != 0) {
-        daysWatched = (watchedTime / (60 * 24)).toFixed(1);
-        daysWatched = `Days watched: ${daysWatched}`;
-    }
-
-    let footer = "";
-    if (watchedTime) footer += daysWatched + "  ";
-    if (chaptersRead) footer += chaptersRead;
-
-    return discordMessage({
-        name: data.name,
-        url: data.siteUrl,
-        imageUrl: data.avatar.large,
-        description: striptags(data.about),
-        footer: footer
-    });
+// Define our query variables and values that will be used in the query request
+var variables = {
+    search: 'Sensorless'
 };
 
 
+async function user() {
+    const obj = await anilistApi(query, variables);
+    console.log(obj);
+    return obj;
+}
 
-module.exports = {
-    data: data,
-    async execute(interaction) {
-        await interaction.JSON.stringify(search);
-    }
-};
+user();
+
+
+// const search = async searchArg => {
+//     const response = await api(query, {
+//         search: searchArg
+//     });
+
+//     if (response.error) {
+//         return response;
+//     }
+
+//     const data = response.User;
+//     console.log(data);
+//     const watchedTime = data.statistics.anime.minutesWatched;
+//     const chaptersRead = data.statistics.manga.chaptersRead;
+
+//     const chaptersString = chaptersRead != 0 ? `Chapters read ${chaptersRead}` : "";
+
+//     let daysWatched = "";
+//     if (watchedTime != 0) {
+//         daysWatched = (watchedTime / (60 * 24)).toFixed(1);
+//         daysWatched = `Days watched: ${daysWatched}`;
+//     }
+
+//     let footer = "";
+//     if (watchedTime) footer += daysWatched + "  ";
+//     if (chaptersRead) footer += chaptersRead;
+
+//     return discordMessage({
+//         name: data.name,
+//         url: data.siteUrl,
+//         imageUrl: data.avatar.large,
+//         description: striptags(data.about),
+//         footer: footer
+//     });
+// };
+
+
+
+// module.exports = {
+//     data: data,
+//     async execute(interaction) {
+//         await interaction.JSON.stringify(search);
+//     }
+// };
+
