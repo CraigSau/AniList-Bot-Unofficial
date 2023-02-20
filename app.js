2c7184df2eb797d7d9154b1b73338901130caba8
CraigSau <37848439+CraigSau@users.noreply.github.com>
2023-02-20 08:41:01 -0600

    Created Embed, consolidated api calls
    Successfully moved data from the API into an embedded message.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const token = process.env.DISCORD_TOKEN;
@@ -29,11 +29,11 @@ for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
-        console.log(client.commands.set(command.data.name, command));
+        client.commands.set(command.data.name, command);
     } else {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
-    client.commands.set(command.data.name, command);
+    //console.log(client.commands.set(command.data.name, command));
 }
 
 //console.log(client.guilds.client)
--- commands/user-profile.js
+++ commands/user-profile.js
@@ -15,70 +15,76 @@ var variables = {
     search: 'Sensorless'
 };
 
-// async function userAbout() {
-//     const obj = await anilistApi(query, userId)
-//         .then((response) => response.User.about)
-//         .catch((err) => console.log(err));
-//     return String(striptags(obj));
-// }
-
-// async function userDaysWatched() {
-//     const obj = await anilistApi(query, userId)
-//         .then((response) => response.User.statistics.anime.minutesWatched)
-//         .catch((err) => console.log(err));
-//     const days = obj / 1440;
-//     return String(days);
-// }
-
-// async function userChaptersRead() {
-//     const obj = await anilistApi(query, userId)
-//         .then((response) => response.User.statistics.manga.chaptersRead)
-//         .catch((err) => console.log(err));
-//     return String(obj);
-// }
-
-// async function userProfilePic() {
-//     const obj = await anilistApi(query, userId)
-//         .then((response) => response.User.avatar.large)
-//         .catch((err) => console.log(err));
-//     return String(obj);
-// }
-
 async function getUser() {
+    let obj = null
     try {
-        const obj = await anilistApi(query, variables)
+        obj = await anilistApi(query, variables)
     } catch (e) {
         console.log(e)
     }
     return obj.User
 }
 
-async function createEmbed() {
-    const user = await getUser();
-    const aboutString = striptags(user.about);
-    const daysWatched = user.minutesWatched / 1440;
-    daysWatched = String(daysWatched);
-    const chaptersRead = user.chaptersRead;
-    chaptersRead = String(chaptersRead);
-    const profilePic = user.avatar.large;
-    const userEmbed = new EmbedBuilder()
-        .setTitle(`${data.options.User}'s Anilist`)
-        //TODO - change the user to not hardcoded
-        .setURL(`https://anilist.co/user/Sensorless/`)
-        .setThumbnail(profilePic)
-        .addFields(
-            { name: 'About', value: aboutString },
-            { name: 'Days Watched', value: daysWatched },
-            { name: 'Chapters Read', value: chaptersRead }
-        )
-    return userEmbed
-}
+// async function createEmbed() {
+//     const user = await getUser();
+//     let aboutString = '';
+//     if (user.about != null) {
+//         aboutString = striptags(user.about);
+//     } else {
+//         aboutString = 'No about found'
+//     }
+//     let daysWatched = parseInt(user.statistics.anime.minutesWatched / 1440);
+//     daysWatched = String(daysWatched);
+//     let chaptersRead = user.statistics.manga.chaptersRead;
+//     chaptersRead = String(chaptersRead);
+//     const profilePic = user.avatar.large;
+//     const userEmbed = new EmbedBuilder()
+//         .setTitle(`${data.options.getUser('user')}'s Anilist`)
+//         //TODO - change the user to not hardcoded
+//         .setURL(`https://anilist.co/user/Sensorless/`)
+//         .setThumbnail(profilePic)
+//         .addFields(
+//             { name: 'About', value: aboutString },
+//             { name: 'Days Watched', value: daysWatched },
+//             { name: 'Chapters Read', value: chaptersRead }
+//         )
+//     return userEmbed
+// }
 
 
 module.exports = {
     data: data,
     async execute(interaction) {
+        const userName = interaction.options.getUser('user');
+        async function createEmbed() {
+            const user = await getUser();
+            let aboutString = '';
+            if (user.about != null) {
+                aboutString = striptags(user.about);
+            } else {
+                aboutString = 'No about found'
+            }
+            let daysWatched = parseInt(user.statistics.anime.minutesWatched / 1440);
+            daysWatched = String(daysWatched);
+            let chaptersRead = user.statistics.manga.chaptersRead;
+            chaptersRead = String(chaptersRead);
+            const profilePic = user.avatar.large;
+            const aniListName = user.name;
+            const userEmbed = new EmbedBuilder()
+                .setTitle(`${aniListName}'s Anilist`)
+                //TODO - change the user to not hardcoded
+                .setURL(`https://anilist.co/user/Sensorless/`)
+                .setThumbnail(profilePic)
+                .addFields(
+                    { name: 'About', value: aboutString },
+                    { name: 'Days Watched', value: daysWatched },
+                    { name: 'Chapters Read', value: chaptersRead }
+                )
+            return userEmbed
+        }
+
         await interaction.reply({ embeds: [await createEmbed()] });
+        await interaction.followUp(`${userName}'s Profile`)
     },
 };
 
